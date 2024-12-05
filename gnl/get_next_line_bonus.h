16152e0088ba64686ee58f6f438d5d478aa20c04
thesmartgirl <61881525+thesmartgirl@users.noreply.github.com>
2024-12-05 19:03:33 +0300

    Add files via upload

---
+++ gnl/get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ataan <ataan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 19:27:49 by ataan             #+#    #+#             */
+/*   Updated: 2024/11/20 17:07:44 by ataan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+#include "../libft.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	*get_next_line(int fd);
+char	*gnl_strjoin(char *s1, char *s2);
+
+#endif
---
+++ gnl/get_next_line_utils_bonus.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ataan <ataan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 19:28:02 by ataan             #+#    #+#             */
+/*   Updated: 2024/11/19 13:40:33 by ataan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*gnl_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str != NULL)
+	{
+		while (s1[i])
+		{
+			str[i + j] = s1[i];
+			i++;
+		}
+		while (s2[j])
+		{
+			str[i + j] = s2[j];
+			j++;
+		}
+		str[i + j] = '\0';
+	}
+	free(s1);
+	return (str);
+}
+
